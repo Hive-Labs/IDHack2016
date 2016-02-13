e0ee518c0b36c40c9ec628b85dff3bc202858965
rkrishnan <rohit.krishnan@stre.am>
2016-02-13 13:20:03 -0500

    port

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('port', 80 || PORT);
+app.set('port', PORT || 80);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
